0a1bc01421fa640afe880efe13bdd3f22a7cce84
likuntao <15811515011@gpcmarket.cn>
2018-01-03 17:46:36 +0800

    pdf

--- src/views/friends/detailFriend.vue
+++ src/views/friends/detailFriend.vue
@@ -16,8 +16,8 @@
           </el-form-item>
           <el-form-item label="性别：" >
             <el-select v-model="info.sex" :disabled="disabled">
-              <el-option :label = "boy" value = "男"></el-option>
-              <el-option :label = "girl" value = "女"></el-option>
+              <el-option :label = "boy" value = "1"></el-option>
+              <el-option :label = "girl" value = "2"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="联系电话：" >
@@ -36,78 +36,75 @@
       </el-collapse-item>
       <el-collapse-item title="公司信息" name="2">
         <p>
-          <el-button  @click="editState($event)" v-show='isShow' size="mini" icon="el-icon-edit"></el-button>
-          <el-button  @click="editFriend" v-show='!isShow' size="mini">完成</el-button>
-          <el-button  @click="cancelEdit" v-show='!isShow' size="mini">取消编辑</el-button>
+          <el-button  @click="changeState" v-show='hasShow' size="mini" icon="el-icon-edit"></el-button>
+          <el-button  @click="doneEdit" v-show='!hasShow' size="mini">完成</el-button>
+          <el-button  @click="offEdit" v-show='!hasShow' size="mini">取消编辑</el-button>
         </p>
         <el-form :label-position="right" label-width="100px">
-          <el-form-item label="公司名称：" :disabled="disabled">
-            <el-select v-model="value" placeholder="请选择公司名称">
+          <el-form-item label="公司名称：">
+            <el-select v-model="info.companyId" :disabled="able" placeholder="请选择">
               <el-option
                 v-for="item in companyList"
                 :key="item.id"
                 :label="item.companyName"
-                :value="item.companyName">
+                :value="item.id">
               </el-option>
             </el-select>
           </el-form-item>
           <div class="tip">找不到公司？尝试 <span class="tips" @click = "reFresh"> 刷新本页</span> 或者 <span class="tips" @click = "createCompany">新建一个公司</span></div>
           <el-form-item label="部门：">
-            <el-input v-model="info.department" :disabled="disabled"></el-input>
+            <el-input v-model="info.department" :disabled="able"></el-input>
           </el-form-item>
           <el-form-item label="职位：">
-            <el-input v-model="info.position" :disabled="disabled"></el-input>
+            <el-input v-model="info.position" :disabled="able"></el-input>
           </el-form-item>
         </el-form>
       </el-collapse-item>
       <el-collapse-item title="认证信息" name="3">
-        <div>简化流程：设计简洁直观的操作流程；</div>
-        <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-        <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
       </el-collapse-item>
     </el-collapse>
     <el-col :span="24" class = "total_info"><span>统计信息:</span></el-col>
 
-    <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row>
-      <el-table-column align="center" label='手机号' width="95">
+    <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row >
+      <el-table-column  label='手机号' min-width="140" align = "center">
         <template slot-scope="scope">
           {{scope.row.mobile}}
         </template>
       </el-table-column>
-      <el-table-column label="用户姓名">
+      <el-table-column label="用户姓名" min-width="140" align = "center">
         <template slot-scope="scope">
           {{scope.row.realname}}
         </template>
       </el-table-column>
-      <el-table-column label="注册时间" width="110" align="center">
+      <el-table-column label="注册时间" width="110" align="center" min-width="140">
         <template slot-scope="scope">
-          {{scope.row.createTime}}
+          {{scope.row.createTime | date}}
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="最近登录时间" width="110" align="center">
+      <el-table-column class-name="status-col" label="最近登录时间" min-width="140" align="center">
         <template slot-scope="scope">
           <i class="el-icon-time"></i>
-          <span>{{scope.row.lastLoginTime}}</span>
+          <span>{{scope.row.lastLoginTime | date}}</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="发布数" width="200">
+      <el-table-column align="center" prop="created_at" label="发布数" min-width="120">
         <template slot-scope="scope">
           <span>{{scope.row.publishCount}}</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="评论数" width="200">
+      <el-table-column align="center" prop="created_at" label="评论数" min-width="120">
         <template slot-scope="scope">
           <span>{{scope.row.commentCount}}</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="分享数" width="200">
+      <el-table-column align="center" prop="created_at" label="分享数" min-width="120">
         <template slot-scope="scope">
           <span>{{scope.row.shareCount}}</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="冻结状态" width="200">
+      <el-table-column align="center" prop="created_at" label="冻结状态" min-width="140">
         <template slot-scope="scope">
-          <span>{{scope.row.status}}</span>
+          <span>{{scope.row.status | state}}</span>
         </template>
       </el-table-column>
     </el-table>
@@ -125,11 +122,11 @@
         listLoading: true,
         info: 'hi',
         input: '',
-        value: '',
         activeNames: ['1'],
         disabled: true,
+        able: true,
         isShow: true,
-        isShoww: true,
+        hasShow: true,
         companyList: [],
         right: 'right',
         boy: '男',
@@ -144,6 +141,27 @@
           deleted: 'danger'
         }
         return statusMap[status]
+      },
+      date(input) {
+        var d = new Date(input)
+        var year = d.getFullYear()
+        var month = d.getMonth() + 1
+        var day = d.getDate() < 10 ? '0' + d.getDate() : '' + d.getDate()
+        return year + '-' + month + '-' + day
+      },
+      sexFilter(value) {
+        if (value === 1) {
+          return '男'
+        } else if (value === 2) {
+          return '女'
+        }
+      },
+      state(value) {
+        if (value === 1) {
+          return '未冻结'
+        } else if (value === 2) {
+          return '冻结'
+        }
       }
     },
     created() {
@@ -159,9 +177,15 @@
           this.disabled = true
         }
       },
+      changeState() {
+        this.hasShow = !this.hasShow
+        if (!this.hasShow) {
+          this.able = false
+        } else {
+          this.able = true
+        }
+      },
       editFriend() {
-        console.log('addFriend')
-
         var params = {
           'companyId': '2',
           'companyName': '',
@@ -189,18 +213,87 @@
           'sort': '',
           'startDate': '',
           'startTime': '2017-12-25',
-          'status': ''
+          'status': '',
+          'companyId' : this.info.companyId,
+          'department': this.info.department,
+          'position': this.info.position
         }
         editFriend(params).then(response => {
           // 后面需要改一下
           console.log(response.data)
           this.info = response.data || this.info
         })
-        this.hide()
+        this.isShow = !this.isShow
+        if (this.isShow) {
+          this.disabled = true
+        } else {
+          this.disabled = false
+        }
+      },
+      doneEdit() {
+        console.log('done')
+        var params = {
+          'companyId': '2',
+          'companyName': '',
+          'creator': '',
+          'department': '',
+          'endDate': '',
+          'endTime': '',
+          'id': this.id,
+          'idcard': this.info.idcard,
+          'introduction': this.info.introduction,
+          'isCompanyPublic': true,
+          'mobile': this.info.mobile,
+          'modifier': '',
+          'modifyTime': '',
+          'nickname': this.info.nickname,
+          'order': '',
+          'pageNum': 1,
+          'pageSize': 10,
+          'password': '',
+          'position': '',
+          'qq': this.info.qq,
+          'realname': this.info.realname,
+          'searchWord': '',
+          'sex': this.info.sex,
+          'sort': '',
+          'startDate': '',
+          'startTime': '2017-12-25',
+          'status': '',
+          'companyId' : this.info.companyId,
+          'department': this.info.department,
+          'position': this.info.position
+        }
+        editFriend(params).then(response => {
+          // 后面需要改一下
+          console.log(response.data)
+          this.info = response.data || this.info
+        })
+        this.hasShow = !this.hasShow
+        if (this.hasShow) {
+          this.able = true
+        } else {
+          this.able = false
+        }
       },
       cancelEdit() {
+        this.fetchData()
         console.log('cancel')
-        this.hide()
+        this.isShow = !this.isShow
+        if (this.isShow) {
+          this.disabled = true
+        } else {
+          this.disabled = false
+        }
+      },
+      offEdit() {
+        this.fetchData()
+        this.hasShow = !this.hasShow
+        if (this.hasShow) {
+          this.able = true
+        } else {
+          this.able = false
+        }
       },
       hide() {
         this.isShow = !this.isShow
@@ -229,10 +322,13 @@
         })
       },
       createCompany() {
-        this.value = '未知'
+        if (this.able) {
+          return
+        }
+        this.info.companyId = '未知'
       },
       reFresh() {
-        window.location.reload()
+        this.getCompany();
       }
     }
   }
